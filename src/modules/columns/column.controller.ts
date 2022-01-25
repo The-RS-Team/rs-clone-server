ae9bc4b63f1377a1a44abd140de9431f52d3c7ae
KozerenkoRoman <kozerenko@gmail.com>
2022-01-26 00:54:40 +0200

    refactor: delete column controller

--- src/modules/columns/column.controller.ts
+++
@@ -1,9 +0,0 @@
-import {Controller} from '@nestjs/common';
-import {ColumnService} from './column.service';
-
-@Controller('columns')
-export class ColumnController {
-    constructor(private readonly columnsService: ColumnService) {
-    }
-
-}
