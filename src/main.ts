9edda2fec152db0c51b6ee75275aa60fa1a47a14
KozerenkoRoman <kozerenko@gmail.com>
2022-02-01 22:33:40 +0200

    feat: add bodyParser to config

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
 import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, {
@@ -15,6 +16,12 @@ async function bootstrap() {
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('swagger', app, document);
     app.setGlobalPrefix('');
+    app.use(bodyParser.json({limit: '10mb'}));
+    app.use(bodyParser.urlencoded({
+        limit: '10mb',
+        parameterLimit: 100000,
+        extended: true
+    }));
     await app.listen(process.env.PORT || 3000);
 }
 
